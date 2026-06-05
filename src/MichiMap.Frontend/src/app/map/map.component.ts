2018c97d2dc6144cb1a0d11fd9d3300bff2a4392
Ty <haytyler7@gmail.com>
2026-06-04 23:16:07 -0400

    Update map.component.ts

--- src/MichiMap.Frontend/src/app/map/map.component.ts
+++ src/MichiMap.Frontend/src/app/map/map.component.ts
@@ -48,9 +48,11 @@ export class MapComponent implements OnInit, OnDestroy {
       zoomControl: true
     });
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      maxZoom: 18
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors ' +
+        '&copy; <a href="https://carto.com/attributions">CARTO</a>',
+      maxZoom: 19
     }).addTo(this.leafletMap);
 
     for (const type of Object.keys(EVENT_LAYER_CONFIG) as EventType[]) {
